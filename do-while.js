5e5097a680c6ff992ae5dd5b6fac1fc1daa58a00
toha245 <mdabdulawal2001@gmail.com>
2025-09-30 00:09:50 +0600

    first commit

---
+++ do-while.js
@@ -0,0 +1,19 @@
+// almost us korbo na 
+// eta while loop er akti modified version
+
+// let i = 10;
+// while(i <5){
+//     console.log(i);
+//     i++;
+// }
+
+
+let n = 10;
+do{
+    console.log(n);
+    n++;
+}
+while(n < 5)
+
+
+// console.log();
---
+++ for-sum.js
@@ -0,0 +1,9 @@
+let sum = 0;
+for(let i = 11; i<=20; i++){
+    sum = sum + i;
+    console.log(i);
+}
+
+console.log('sum of numbers from 11 to 20 is', sum);
+
+// console.log();
