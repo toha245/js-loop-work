79bc7e4a13687101e859fe2b07887cebfa37959b
toha245 <mdabdulawal2001@gmail.com>
2025-10-08 23:44:04 +0600

    second commit

---
+++ module_19_practice.js
@@ -0,0 +1,101 @@
+// for loop tasks
+// task 1
+// let message = 'I will invest at least 6 hrs every single day for next 60 days!';
+// for(let i = 1; i <= 60; i++){
+//     console.log(i + '.' + message);
+// }
+
+// task 2
+// subtask 1 odd numbers 61 to 100
+// for(let i = 61; i <= 100; i++){
+//     if(i % 2 === 1){
+//         console.log(i);
+//     }
+// }
+
+// subtask 2
+// even numbers 78 to 98
+// for(let i = 78; i <= 98; i++){
+//     if(i % 2 === 0){
+//         console.log(i);
+//     }
+// }
+
+// task 3
+// subtask 1 sum of odd numbers 
+// let oddSum = 0;
+// for(let i = 91; i <=129; i++ ){
+//     if(i % 2 !== 0){
+//         console.log('odd numbers' + ' : ' + i)
+//         oddSum = oddSum + i;
+//         console.log('sum of odd numbers after every loop' + ' : ' + oddSum);
+//     }
+// }
+// console.log('total of odd' + ' : ' + oddSum);
+
+
+// subtask 2 sum of even numbers 
+// let evenSum = 0;
+// for(let i = 51; i <=85; i++ ){
+//     if(i % 2 !== 1){
+//         console.log('even number' + ' : ' + i)
+//         evenSum = evenSum + i;
+//         console.log('sum of even numbers after every loop' + ' : ' + evenSum);
+//     }
+// }
+// console.log('total of even numbers' + ' : ' + evenSum);
+
+
+// task 4  multiplication table for number 9
+// Specify the number for which to generate the multiplication table
+// const number = 9;
+// // Loop from 1 to 10 to generate the table
+// for (let i = 1; i <= 10; i++){
+//   // Calculate the product
+//   const result = number * i;
+//   // Print the multiplication expression and its result to the console
+//   console.log(`${number} * ${i} = ${result}`);
+// }
+
+// let number = 11;
+// for(let i = 1; i <= 10; i++){
+//     let result = number * i;
+//     console.log(`${number} * ${i} = ${result}`);
+// }
+
+
+// count down from 81 to 65
+// for(let i = 81; i >= 65; i--){
+//     console.log(i);
+// }
+
+
+// while loop tasks
+// task 1
+// let message = 'I will invest at least 6 hrs every single day for next 60 days!';
+// let i = 1;
+// while(i <= 60){
+//     console.log(`${i}` + '.' + message);
+//     i++;
+// }
+
+
+// task 2
+// subtask 1 odd numbers 61 to 100
+// let oddNumber = 61;
+// while(oddNumber <= 100){
+//     if(oddNumber % 2 === 1){
+//         console.log(oddNumber)
+//     }
+//     oddNumber++;
+// } 
+
+// subtask 2  even numbers from 78 to 98
+// let evenNumber = 78;
+// while(evenNumber <= 98){
+//     if(evenNumber % 2 !== 1){
+//         console.log(evenNumber);
+//     }
+//     evenNumber ++;
+// }
+
