82bd31678171fd68e19d8306f5e85a426266d9e8
toha245 <mdabdulawal2001@gmail.com>
2025-11-04 02:26:34 +0600

    last commit

--- module_19_practice.js
+++ module_19_practice.js
@@ -63,11 +63,12 @@
 //     console.log(`${number} * ${i} = ${result}`);
 // }
 
-
+//task 5
 // count down from 81 to 65
 // for(let i = 81; i >= 65; i--){
 //     console.log(i);
 // }
+
 
 
 // while loop tasks
@@ -90,7 +91,7 @@
 //     oddNumber++;
 // } 
 
-// subtask 2  even numbers from 78 to 98
+// subtask 2 even numbers from 78 to 98
 // let evenNumber = 78;
 // while(evenNumber <= 98){
 //     if(evenNumber % 2 !== 1){
@@ -102,3 +103,143 @@
 
 // task 3
 // subtask 1 sum of odd numbers 
+// let oddNumbers = 81;
+// let sum = 0;
+// while(oddNumbers <= 131){
+//     if(oddNumbers % 2 !== 0){
+//         sum = sum + oddNumbers;
+//     }
+//     oddNumbers ++;
+// }
+// console.log(sum);
+
+
+// subtask 2 sum of even numbers 
+// let i = 206;
+// let sum = 0;
+// while(i <= 311){
+//     if(i % 2 === 0){
+//         sum += i;
+//     }
+//     i ++;
+// }
+// console.log(sum);
+
+// system 2
+// let i = 206;
+// let sum = 0;
+// while(i <= 311){
+//     sum += i;
+//     i += 2;
+// }
+// console.log(sum);
+
+
+// task 4
+// multiplication table for number 5 with while loop
+// let n = 1;
+// while(n <= 10){
+//     let result = n * 5;
+//     console.log('5' + ' * ' + n + ' = ' + result);
+//     n++;
+// }
+
+// system 2
+// let n = 1;
+// let mult = 5;
+// while(n <= 10){
+//     let result = n * mult;
+//     console.log(mult + ' * ' + n + ' = ' + result);
+//     n++;
+// }
+
+
+// task 5
+// countdown 21 to 15
+// let i = 21;
+// let n = 15;
+// while(i >= n){
+//     console.log(i);
+//     i --;
+// }
+
+
+// continue tasks
+// task 1: Write a loop to print even numbers from 1 to 40. Use continue to skip odd numbers.
+// for(i = 1; i <= 40; i++){
+//     if(i % 2 === 1){
+//         continue;
+//     }
+//     console.log(i);
+// }
+
+// task 2: display odd number from 55 to 85 and skip the numbers divisible by 5.
+
+// for(i = 55; i <= 85; i++){
+//     if(i % 5 === 0){
+//         continue;
+//     }
+//     else if(i % 2 === 0){
+//         continue;
+//     }
+//     else{
+//         console.log(i);
+//     }
+// }
+
+
+// system 2
+// for(i = 55; i <= 85; i++){
+//     if(i % 5 === 0 || i % 2 === 0){
+//         continue;
+//     }
+//     console.log(i);
+// }
+
+
+// break tasks
+// task 1 : Write a loop 1 to 200. Use break to exit the loop once you find 100.
+// for(i = 1; i <= 200; i++){
+//     console.log(i);
+//     if(i === 100){
+//         break;
+//     }
+// }
+
+
+// task 2 : Write a while loop that adds numbers starting from 1, but stops (using break) as soon as the sum reaches or exceeds 100
+// let i = 1;
+// let sum = 0;
+// while(true){
+//     sum += i;
+//     console.log(i);
+//     if(sum >= 100){
+//         break;
+//     }
+//     i++;
+// }
+
+
+// task 3 : Write a loop that goes from 1 to 100, but stops (using break) when it encounters the first square number (like 4, 9, 16, etc.)
+
+// for(let i = 1; i <= 100; i++){
+//     // check if i is a square number
+//     let sqrt = Math.sqrt(i);
+//     if(Number.isInteger(sqrt)){  // যদি square root integer হয়, i square number
+//         console.log("First square number found:", i);
+//         break;      
+//     }
+// }
+
+let i = 1;
+while(i <= 100){
+    let sqrt = Math.sqrt(i);
+    if(Number.isInteger(sqrt)){
+        console.log("First square number found:", i);
+        break;
+    }
+    i++;
+}
+
+
+// console.log();
